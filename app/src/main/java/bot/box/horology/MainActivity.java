32983172adf72854ae974efee6aef9a5266088d9
me40 <bhan@quantum4u.in>
2018-08-25 20:19:32 +0530

    //third commit //unused comments removed

--- app/src/main/java/bot/box/horology/MainActivity.java
+++ app/src/main/java/bot/box/horology/MainActivity.java
@@ -1,11 +1,8 @@
 package bot.box.horology;
 
 import android.app.Dialog;
-import android.content.Intent;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 import android.widget.LinearLayout;
